749e9113a158b122825819044af40e8dce552c23
d18c7db@hotmail.com <d18c7db@hotmail.com@e1b4b417-ffd7-565e-1f90-023a6b689ca6>
2013-05-09 07:39:18 +0000

    Added more ROM folders, schematics for Pengo and Packman hardware

--- trunk/scripts/build_roms_pengo.bat
+++ trunk/scripts/build_roms_pengo.bat
@@ -2,6 +2,10 @@
 
 REM SHA1 sums of files required
 
+REM 680eab0e1204c9b74adc11588461651b474021bb pr1633.78
+REM 3fcd66610fcaee814953a115bf5e04788923181f pr1634.88
+REM 563c9770028fe39188e62630711589d6ed242a66 pr1635.51
+REM 0c4d0bee858b97632411c440bea6948a74759746 pr1636.70
 REM e542bcc28f292be9a0a29d949de726e0b55e654a ep1640.92
 REM 0930de17a763a527057f60783a92662b09554426 ep1689c.8
 REM 4c97529e61eeca5d94938b1dfbeac41bf8cbaf7d ep1690b.7
@@ -12,10 +16,7 @@ REM 7b47aec61593efd758e2a031f72a854bb0ba8af1 ep1694b.20
 REM bdec535e486b43a8f5550334beff423eeace10b2 ep1695.105
 REM 207ed466546f40ca60a38031b83aef61446902e2 ep5118b.32
 REM fec7236b3dee2ea6e39c68440a6d2d9e3f72675a ep5119c.31
-REM 680eab0e1204c9b74adc11588461651b474021bb pr1633.78
-REM 3fcd66610fcaee814953a115bf5e04788923181f pr1634.88
-REM 563c9770028fe39188e62630711589d6ed242a66 pr1635.51
-REM 0c4d0bee858b97632411c440bea6948a74759746 pr1636.70
+
 
 set rom_path_src=..\roms\pengo
 set rom_path=..\build
@@ -28,12 +29,15 @@ copy /b/y %rom_path_src%\ep1693b.21 + %rom_path_src%\ep1694b.20 + %rom_path_src%
 
 REM generate RTL code for small PROMS
 %romgen_path%\romgen %rom_path_src%\pr1635.51     PROM1_DST  9 c     > %rom_path%\prom1_dst.vhd
+%romgen_path%\romgen %rom_path_src%\pr1636.70     PROM3_DST  7 a     > %rom_path%\prom3_dst.vhd
 %romgen_path%\romgen %rom_path_src%\pr1634.88     PROM4_DST 10 c     > %rom_path%\prom4_dst.vhd
 %romgen_path%\romgen %rom_path_src%\pr1633.78     PROM7_DST  5 c     > %rom_path%\prom7_dst.vhd
 
 REM generate RAMB structures for larger ROMS
 %romgen_path%\romgen %rom_path%\gfx1.bin          GFX1      14 l r e > %rom_path%\gfx1.vhd
 %romgen_path%\romgen %rom_path%\main1.bin         ROM_PGM_0 14 l r e > %rom_path%\rom0.vhd
+
+
 %romgen_path%\romgen %rom_path%\main2.bin         ROM_PGM_1 14 l r e > %rom_path%\rom1.vhd
 
 echo done
